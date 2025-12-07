5c6c66d182257169083955573d2aed200b45701a
Lisa <edolgikh03@gmail.com>
2025-12-07 14:23:42 +0300

    update Test Clang-Tidy integration

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@ int main(int argc, char* argv[]) {
     double sigma = 0.2;
     unsigned long long paths = 1'000'000;
     unsigned int steps = 252;
+    // Trigger build
     int* bad_pointer = (int*)0;
 
     // Парсинг аргументов
